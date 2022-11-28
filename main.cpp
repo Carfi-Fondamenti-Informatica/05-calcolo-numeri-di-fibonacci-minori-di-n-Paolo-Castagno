049c9691e228fcd2d5cf1ba19c073a92ce23023e
Paolo-Castagno <117978458+Paolo-Castagno@users.noreply.github.com>
2022-11-28 09:42:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-   return 0;
+int main(){
+
+    int n, p=0, s=1, i=1;
+    cin >> n;
+    cout << s << endl;
+    while (i++ <=n){
+        cout<<p+s<<endl;
+        if(p<s){
+            p+=s;
+        }else{
+            s+=p;
+        }
+        if ((p+s)>n)
+            break;
+    }
+
+    return 0;
 }
